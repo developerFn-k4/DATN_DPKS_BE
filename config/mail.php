a7090ce1d2c81d3468eb4a69d5db02fd94b376b3
hungw1234 <huy121212cm@gmail.com>
2026-03-18 00:18:12 +0700

    Update changes before pushing to hung-dev

--- config/mail.php
+++ config/mail.php
@@ -111,7 +111,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'huy121212cm@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
